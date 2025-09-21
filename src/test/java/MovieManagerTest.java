278048040569aa24acf89e533303e75924e9d22e
nasik_ananasik <borodina-nas@mail.ru>
2025-09-21 17:50:18 +0300

    add tests 3

--- src/test/java/MovieManagerTest.java
+++ src/test/java/MovieManagerTest.java
@@ -16,13 +16,15 @@ public class MovieManagerTest {
     MovieItem item9 = new MovieItem(9, "Плохие парни", "Комедия");
 
     @Test
-    public void shouldAddMovie() {
+    public void shouldDisplayFiveMoviesByDefault() { // Выводит по умолчанию 5 фильмов
         MovieManager movie = new MovieManager();
         movie.addMovie(item1);
         movie.addMovie(item2);
         movie.addMovie(item3);
         movie.addMovie(item4);
         movie.addMovie(item5);
+        movie.addMovie(item6);
+        movie.addMovie(item7);
 
         MovieItem[] expected = {item1, item2, item3, item4, item5};
         MovieItem[] actual = movie.findAll();
@@ -31,46 +33,65 @@ public class MovieManagerTest {
     }
 
     @Test
-    public void lessMoviesAdded() {
+    public void shouldDisplayFewerMoviesThanTheDefault() { // Выводит фильмы меньше чем указанно по умолчанию
         MovieManager movie = new MovieManager();
         movie.addMovie(item1);
         movie.addMovie(item2);
-        movie.addMovie(item6);
+        movie.addMovie(item3);
+        movie.addMovie(item4);
 
-        MovieItem[] expected = {item1, item2, item6};
+        MovieItem[] expected = {item1, item2, item3, item4};
         MovieItem[] actual = movie.findAll();
 
         Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void moreMoviesAdded() {
+    public void shouldDisplayFewerMoviesThanTheDefaultFindLast() { // Выводит фильмы меньше чем указанно по умолчанию в обратном порядке
         MovieManager movie = new MovieManager();
         movie.addMovie(item1);
         movie.addMovie(item2);
-        movie.addMovie(item6);
         movie.addMovie(item3);
         movie.addMovie(item4);
+
+        MovieItem[] expected = {item4, item3, item2, item1};
+        MovieItem[] actual = movie.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldAddMovie() { // Метод добавления
+        MovieManager movie = new MovieManager();
+        movie.addMovie(item1);
+        movie.addMovie(item2);
+
+        MovieItem[] expected = {item1, item2};
+        MovieItem[] actual = movie.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldOutputAnEmptyArray() { // Выводит пустой массив
+        MovieManager movie = new MovieManager();
+
+        MovieItem[] expected = {};
+        MovieItem[] actual = movie.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldAddMovieAndFindLast() { // Выводит 5 фильмов по умолчанию в обратном порядке добавления
+        MovieManager movie = new MovieManager();
+        movie.addMovie(item3);
         movie.addMovie(item5);
-        movie.addMovie(item7);
+        movie.addMovie(item1);
+        movie.addMovie(item4);
+        movie.addMovie(item2);
         movie.addMovie(item8);
-        movie.addMovie(item9);
-
-        MovieItem[] expected = {item1, item2, item6, item3, item4};
-        MovieItem[] actual = movie.findAll();
-
-        Assertions.assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldAddMovieAndFindLast() {
-        MovieManager movie = new MovieManager();
-        movie.addMovie(item3);
-        movie.addMovie(item5);
-        movie.addMovie(item1);
-        movie.addMovie(item4);
-        movie.addMovie(item2);
-        movie.addMovie(item2);
+        movie.addMovie(item7);
 
         MovieItem[] expected = {item2, item4, item1, item5, item3};
         MovieItem[] actual = movie.findLast();
@@ -79,7 +100,7 @@ public class MovieManagerTest {
     }
 
     @Test
-    public void movieLessThanLimit() {
+    public void movieLessThanLimit() { // Выодит фильмы меньше установленной нормы
         MovieManager movie = new MovieManager(3);
         movie.addMovie(item3);
         movie.addMovie(item5);
@@ -95,24 +116,63 @@ public class MovieManagerTest {
     }
 
     @Test
-    public void movieMoreThanLimit() {
-        MovieManager movie = new MovieManager(10);
+    public void movieLessThanLimitFindLast() { // Выодит фильмы меньше установленной нормы в обратном порядке
+        MovieManager movie = new MovieManager(3);
         movie.addMovie(item3);
+        movie.addMovie(item5);
+        movie.addMovie(item1);
+        movie.addMovie(item4);
+        movie.addMovie(item2);
+        movie.addMovie(item2);
+
+        MovieItem[] expected = {item1, item5, item3};
+        MovieItem[] actual = movie.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void movieMoreThanLimit() { // Выодит фильмы больше установленной нормы
+        MovieManager movie = new MovieManager(8);
+        movie.addMovie(item8);
         movie.addMovie(item5);
         movie.addMovie(item1);
         movie.addMovie(item4);
         movie.addMovie(item2);
         movie.addMovie(item6);
         movie.addMovie(item9);
+        movie.addMovie(item7);
+        movie.addMovie(item3);
 
-        MovieItem[] expected = {item3, item5, item1, item4, item2, item6, item9};
+
+        MovieItem[] expected = {item8, item5, item1, item4, item2, item6, item9, item7};
         MovieItem[] actual = movie.findAll();
 
         Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void movieEqualLimit() {
+    public void movieMoreThanLimitFindLast() { // Выодит фильмы больше установленной нормы в обрятном порядке
+        MovieManager movie = new MovieManager(8);
+        movie.addMovie(item8);
+        movie.addMovie(item5);
+        movie.addMovie(item1);
+        movie.addMovie(item4);
+        movie.addMovie(item2);
+        movie.addMovie(item6);
+        movie.addMovie(item9);
+        movie.addMovie(item7);
+        movie.addMovie(item3);
+
+
+        MovieItem[] expected = {item7, item9, item6, item2, item4, item1, item5, item8};
+        MovieItem[] actual = movie.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void movieEqualLimit() { // Выодит фильмы равные лимиту
         MovieManager movie = new MovieManager(5);
         movie.addMovie(item3);
         movie.addMovie(item5);
@@ -123,6 +183,48 @@ public class MovieManagerTest {
 
         MovieItem[] expected = {item3, item5, item6, item4, item2};
         MovieItem[] actual = movie.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void movieEqualLimitFindLast() { // Выодит фильмы равные лимиту в обратном порядке
+        MovieManager movie = new MovieManager(5);
+        movie.addMovie(item3);
+        movie.addMovie(item5);
+        movie.addMovie(item6);
+        movie.addMovie(item4);
+        movie.addMovie(item2);
+        movie.addMovie(item1);
+
+        MovieItem[] expected = {item2, item4, item6, item5, item3};
+        MovieItem[] actual = movie.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldDisplayMovieLessThanSpecified() { // Выодит фильмы меньше установленного колличества
+        MovieManager movie = new MovieManager(4);
+        movie.addMovie(item3);
+        movie.addMovie(item5);
+        movie.addMovie(item6);
+
+        MovieItem[] expected = {item3, item5, item6};
+        MovieItem[] actual = movie.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldDisplayMovieLessThanSpecifiedFindLast() { // Выодит фильмы меньше установленного колличества в обратном порядк
+        MovieManager movie = new MovieManager(4);
+        movie.addMovie(item3);
+        movie.addMovie(item5);
+        movie.addMovie(item6);
+
+        MovieItem[] expected = {item6, item5, item3};
+        MovieItem[] actual = movie.findLast();
 
         Assertions.assertArrayEquals(expected, actual);
     }
